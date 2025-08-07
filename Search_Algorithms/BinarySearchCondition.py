e67b59c34cbe67f84a994c86aba3521d348a5904
Manojreddy0509 <161306313+Manojreddy0509@users.noreply.github.com>
2025-08-08 02:00:33 +0530

    Create BinarySearch_Condition.py

---
+++ Search_Algorithms/BinarySearchCondition.py
@@ -0,0 +1,19 @@
+# Condition based
+B = [False, False, False, False, True]
+
+def binary_search_condition(arr):
+  N = len(arr)
+  L = 0
+  R = N - 1
+
+  while L < R:
+    M = (L + R) // 2
+
+    if arr[M]:
+      R = M
+    else:
+      L = M + 1
+
+  return L
+
+binary_search_condition(B)
